db81fca2cd849ad04b2d4285db32a90942f9ef69
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-26 14:49:02 +0530

    first commit

---
+++ Customer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_GetSet
+{
+    internal class Customer
+    {/*
+        int id;
+        string name;
+        DateTime mEndDate;
+        string comment;*/
+        /*public Customer()
+        {
+            Console.WriteLine("Customer Constructor");
+            id = -1;
+            name = "not given";
+            mEndDate = DateTime.Now;
+        }
+        public int Id{get{ return id; } set{ id = value;} }
+        public string Name { get { return name; } set { name = value; } }
+        public DateTime MEndDate { get { return mEndDate; } set { mEndDate = value; } }*/
+        //public Customer(int id,string name,DateTime mEndDate)
+        //{
+        //    Console.WriteLine("Customer Constructor");
+        //    this.id = id;
+        //    this.name = name;
+        //    this.mEndDate = mEndDate;
+        //}
+        //public int Id { get { return id; } }
+        //public string Name { get { return name; } }
+        //public DateTime MEndDate { get { return mEndDate; } }
+        /*public Customer(int id, string name, DateTime mEndDate)
+        {
+            Console.WriteLine("Customer Constructor");
+            this.id = id;
+            this.name = name;
+            this.mEndDate = mEndDate;
+        }
+        public int Id { get { return id; } }
+        public string Name { get { return name; } }
+        public DateTime MEndDate { get { return mEndDate; } }
+        public string Comment { set { comment = value; } }
+        public void ReadComment(string passCode)
+        {
+            if(passCode=="admin@123")
+                Console.WriteLine("Comment is : "+comment);
+        }*//*
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { if (value >= 1) { id = value; } else { id = -1; } }
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { if (value.Length >= 6) { name = value; } else { name = "Invalid"; } }
+        }*//*
+        private static double tax;
+        public static double Tax { get { return tax; } set { tax = value; } }
+        private int id;
+        public int Id { get { return id; } set { if (value >= 1) { id = value; } else { id = -1; } } }
+
+        private string name;
+        public string Name { get { return name; } set { if (value.Length >= 6) { name = value; } else { name = "Invalid"; } } }*/
+        int id;
+        public int Id { get { return id; } set { id = value; } }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public double ODLimit { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_GetSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            Customer cust = new Customer();
+                        Console.WriteLine($"{cust.Id},{cust.Name},{cust.MEndDate}");
+                        cust.Id = 34;
+                        cust.Name = "Praveen";
+                        Console.WriteLine();
+                        Console.WriteLine($"{cust.Id},{cust.Name},{cust.MEndDate}");*/
+            //Customer cust = new Customer(12, "Raj", new DateTime(day: 09, month: 02, year: 2024));
+            //Console.WriteLine("Customer Details as follows");
+            //Console.WriteLine($"Customer ID : {cust.Id}\nCustomer Name : {cust.Name}\nMembershipEndDate : {cust.MEndDate}");
+            /*Customer cust = new Customer(12, "Raj", new DateTime(day: 09, month: 02, year: 2024));
+            Console.WriteLine("Customer Details as Follows");
+            Console.WriteLine($"Customer ID : {cust.Id}\nCustomer Name : {cust.Name}\nMembershipEndDate : {cust.MEndDate}");
+            cust.Comment = "I am Delighted with the services provided by the vendor";
+            cust.ReadComment("admin@123");*/
+
+            //Customer cust = new Customer();
+            //Console.WriteLine("Enter Id");
+            //cust.Id = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter Name");
+            //cust.Name = Console.ReadLine();
+            //Console.WriteLine("Customer Details");
+            //Console.WriteLine("ID : "+cust.Id+"\nName : "+cust.Name);
+            /*
+                        Customer cust = new Customer();
+                        Customer cust2 = new Customer();
+                        Console.WriteLine("Enter Tax");
+                        Customer.Tax = double.Parse(Console.ReadLine());
+                        Console.WriteLine("***Customer One***");
+                        Console.WriteLine("Enter Id");
+                        cust.Id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter Name");
+                        cust.Name=Console.ReadLine();
+                        Console.WriteLine("Customer Details");
+                        Console.WriteLine(("ID : "+cust.Id+"\nName : "+cust.Name));
+
+                        Console.WriteLine("***Customer Two***");
+                        Console.WriteLine("Enter Id");
+                        cust2.Id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter Name");
+                        cust2.Name = Console.ReadLine();
+                        Console.WriteLine("Customer Details");
+                        Console.WriteLine(("ID : " + cust2.Id + "\nName : " + cust2.Name));*/
+
+            Customer cust = new Customer();
+            Console.WriteLine("Enter Id");
+            cust.Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            cust.Name = Console.ReadLine();
+            Console.WriteLine("Enter Customer City");
+            cust.City = Console.ReadLine();
+            Console.WriteLine("Enter Customer Over Draft Limit");
+            cust.ODLimit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Customer Detailds as follows");
+            Console.WriteLine($"Id : {cust.Id} Name : {cust.Name}");
+            Console.WriteLine($" City: { cust.City} ODLimit : {cust.ODLimit}");
+        }
+    }
+}
